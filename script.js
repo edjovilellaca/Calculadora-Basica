9b0254952b44db9d0c24187ed2fc5c1f7fff3443
Joel/Zakloni <edjovilellaca@ittepic.edu.mx>
2024-09-06 09:25:01 -0700

    ya ahora si

--- script.js
+++ script.js
@@ -1,34 +1,38 @@
-const campo1 = document.getElementById('txt1');
-const campo2 = document.getElementById('txt2');
-const resul = document.getElementById('txt3');
-
 const btnSumar = document.getElementById('btnSum');
 const btnRestar = document.getElementById('btnRes');
 const btnMultiplicar = document.getElementById('btnMul');
 const btnDividir = document.getElementById('btnDiv');
 
-let num1 = parseFloat(campo1.value);
-let num2 = parseFloat(campo2.innerText);
+const resul = document.getElementById('txt3');
 
 btnSumar.addEventListener('click', ()=>{
-    let res = num1 + num2;
-    console.log(num1);
-    console.log(num2);
+    const num1 = document.getElementById('txt1').value;
+    const num2 = document.getElementById('txt2').value;
+    let res = parseFloat(num1) + parseFloat(num2);
     console.log(res);
-    resul.innerText = res;
+    resul.setAttribute('value', res);
 });
 
 btnRestar.addEventListener('click', ()=>{
-    let res = num1 - num2;
-    resul.innerText = res;
+    const num1 = document.getElementById('txt1').value;
+    const num2 = document.getElementById('txt2').value;
+    let res = parseFloat(num1) - parseFloat(num2);
+    console.log(res);
+    resul.setAttribute('value', res);
 });
 
 btnMultiplicar.addEventListener('click', ()=>{
-    let res = num1 * num2;
-    resul.innerText = res;
+    const num1 = document.getElementById('txt1').value;
+    const num2 = document.getElementById('txt2').value;
+    let res = parseFloat(num1) * parseFloat(num2);
+    console.log(res);
+    resul.setAttribute('value', res);
 });
 
 btnDividir.addEventListener('click', ()=>{
-    let res = num1 / num2;
-    resul.innerText = res;
+    const num1 = document.getElementById('txt1').value;
+    const num2 = document.getElementById('txt2').value;
+    let res = parseFloat(num1) / parseFloat(num2);
+    console.log(res);
+    resul.setAttribute('value', res);
 });
