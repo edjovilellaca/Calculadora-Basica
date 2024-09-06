a267f15fde691be4a8afb0067845bc2e283a34b1
Joel/Zakloni <edjovilellaca@ittepic.edu.mx>
2024-09-06 08:56:45 -0700

    ID's cambiadas en JS

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const campo1 = document.getElementById('campo1');
 const campo2 = document.getElementById('campo2');
 const resul = document.getElementById('resultado');
 
-const btnSumar = document.getElementById('btnSumar');
-const btnRestar = document.getElementById('btnRestar');
-const btnMultiplicar = document.getElementById('btnMultiplicar');
-const btnDividir = document.getElementById('btnDividir');
+const btnSumar = document.getElementById('btnSum');
+const btnRestar = document.getElementById('btnRes');
+const btnMultiplicar = document.getElementById('btnMul');
+const btnDividir = document.getElementById('btnDiv');
 
 let num1 = parseFloat(campo1.innerText);
 let num2 = parseFloat(campo2.innerText);
